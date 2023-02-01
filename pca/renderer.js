cf277193b381da0e1810be5929ef9990df812052
tilastotuusix <95974078+tilastotuusix@users.noreply.github.com>
2023-02-01 10:53:02 +0200

    Add files via upload

--- pca/renderer.js
+++ pca/renderer.js
@@ -1,4 +1,4 @@
-const d3 = require('d3')
+//const d3 = require('d3')
 
 const render = ({index1, index2, width, height, margin, scoreColor, loadingColor, leadColor, loadingOpacity, textSize, circleR}) => {
   return (selection) => {
